68315c26f2abaa75def3e01438f9a038c8763671
Harry Crane <37228043+hwcrane@users.noreply.github.com>
2023-12-06 11:10:22 +0000

    made part 1 one line again

--- day_6/part_1.py
+++ day_6/part_1.py
@@ -1,18 +1,4 @@
 import sys
 from functools import reduce
 
-print(
-    reduce(
-        lambda acc, vals: acc * (vals[-1] - vals[0] + 1),
-        [
-            [i for i in range(1, time) if i * (time - i) > dist]
-            for time, dist in zip(
-                *[
-                    list(map(int, line.strip().split()[1:]))
-                    for line in open(sys.argv[1]).readlines()
-                ]
-            )
-        ],
-        1,
-    )
-)
+print( reduce( lambda acc, vals: acc * (vals[-1] - vals[0] + 1), [ [i for i in range(1, time) if i * (time - i) > dist] for time, dist in zip( *[ list(map(int, line.strip().split()[1:])) for line in open(sys.argv[1]).readlines() ]) ], 1,))
